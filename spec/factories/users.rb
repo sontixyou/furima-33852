b8deb98cb16d6ab78f4211abf7e7d446a550fa3b
kenken <angelbeatsviolin@gmail.com>
2021-02-01 11:31:25 +0900

    ユーザー管理機能提出2

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,7 +5,11 @@ FactoryBot.define do
     password = Faker::Internet.password(min_length: 6)
     password {password}
     password_confirmation {password}
-   
+    first_name  {"太郎"}
+    last_name {"鈴木"}
+    first_name_furigana {"タロウ"}
+    last_name_furigana {"スズキ"}
+    birthday {'1990/09/01'}
 
   end
-end
+end 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,18 +4,12 @@ RSpec.describe User, type: :model do
   describe '#create' do
     before do
       @user = FactoryBot.build(:user)
-      @user.first_name = "太郎"
-      @user.last_name = "鈴木"
-      @user.first_name_furigana = "タロウ"
-      @user.last_name_furigana = "スズキ"
-      @user.birthday.published_at[0] = "1990"
-      @user.birthday.start_date[1] = 1
-      @user.birthday.start_date[2] = 19 
     end
     
     
     
-    it "nicknameとemail、passwordとpassword_confirmationが存在すれば登録できること" do
+    it "nicknameとemail、passwordとpassword_confirmation、first_name, last_name, first_name_furigana, presence: true
+    validates :last_name_furigana, birthdayが存在すれば登録できること" do
       expect(@user).to be_valid
     end
 
@@ -36,5 +30,31 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include("Password can't be blank")
     end
+
+    it "passwordが存在してもpassword_confirmationが空では登録できない" do
+      @user.password_confirmation = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+   
+
+    it "重複したemailが存在する場合登録できない" do
+      @user.save
+      another_user = FactoryBot.build(:user)
+      another_user.email = @user.email
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include("Email has already been taken")
+    end
+
+    it "passwordが5文字以下では登録できない" do
+      @user.password = "00000"
+      @user.password_confirmation = "00000"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end
+
+    
+    
   end
 end
