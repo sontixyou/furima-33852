c166fa50e2177d7b2875f67b2575b5db1263424d
kenken <angelbeatsviolin@gmail.com>
2021-02-09 10:40:37 +0900

    商品編集機能提出３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -56,15 +56,9 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-<<<<<<< Updated upstream
     unless current_user.id == @item.user_id && user_signed_in?
       redirect_to root_path
     end
-=======
-    unless user_signed_in? && current_user.id == @item.user.id
-      redirect_to root_path
-    end  
->>>>>>> Stashed changes
   end  
 end
  
