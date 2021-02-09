3b66bc15a65cb816b7363877a3f8f09a5df55882
kenken <angelbeatsviolin@gmail.com>
2021-02-09 12:15:17 +0900

    商品削除機能提出１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show, :update]
-  before_action :move_to_index, only: [:edit]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :delete]
   
   def index
     @items =Item.all.order("created_at DESC")
@@ -37,6 +37,11 @@ class ItemsController < ApplicationController
       render :edit
     end  
   end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end 
   
   private
   def item_params
