a2e3ff69bd4cbf30f723cc32fdb1b149752248de
kenken <angelbeatsviolin@gmail.com>
2021-02-17 21:15:53 +0900

    購入機能提出４

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -36,8 +36,8 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    item = Item.find(params[:item_id])
-    redirect_to root_path if current_user.id == item.user_id || item.order.present?
+    item_find
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 
   def item_find
