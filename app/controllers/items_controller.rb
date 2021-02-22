ab03a61bf102721e42fa3f8ff71ef5f279b4d0e6
kenken <angelbeatsviolin@gmail.com>
2021-02-22 19:57:12 +0900

    heroku4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,7 +51,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path unless current_user.id == @item.user_id || if @item.order.present?
+    redirect_to root_path if current_user.id == @item.user_id && @item.order.present? || @item.order.present?
     end
   end  
 end
